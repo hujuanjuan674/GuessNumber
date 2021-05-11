a4bf26cce5e95414f40110535cb6f139c70ab67c
hujuanjuan674 <3331828993@qq.com>
2021-05-11 22:12:00 +0800

    number

---
+++ src/Practice/Example.java
@@ -0,0 +1,12 @@
+package Practice;
+
+public class Example {
+    public static void main(String args[]) {
+        Circle a = new Circle();
+        Square b = new Square();
+        Rectangle c = new Rectangle();
+        a.getAera();
+        b.getAera();
+        c.getAera();
+    }
+}
--- src/Practice/Shape.java
+++ src/Practice/Shape.java
@@ -1,57 +1,20 @@
 package Practice;
 
-public  abstract class Shape {
-    public abstract double getArea();
-
-    public static class Pillar {
-        Shape bottom;
-        double height;
-        Pillar(Shape bottom,double height){
-            this.bottom=bottom;
-            this.height=height;
-        }
-        public double getArea(){
-            if(bottom==null){
-                System.out.println("没有底，无法计算");
-                return -1;
-            }
-            return bottom.getArea()*height;
-        }
-
-    }
-
-    public static class Rectangle  extends Shape{
-        double a,b;
-        Rectangle(double a,double b){
-            this.a=a;
-            this.b=b;
-
-        }
-        public double getArea(){
-            return a*b;
-        }
-    }
-
-    public static class Circle extends Shape{
-        double r;
-        Circle(double r){
-            this.r=r;
-        }
-        public double getArea() {
-            return (3.14 * r * r);
-        }
-
-    }
-
-    public static class Square extends Shape{
-        double a,b;
-        Square(double a,double b){
-            this.a=a;
-            this.b=b;
-        }
-        public double getArea(){
-            return a*b;
-        }
-
-    }
+abstract class Shape {
+    double Aera=10;    abstract double getAera();
+}
+class Circle extends Shape{
+    @Override    double getAera() {
+        System.out.println("圆形的面积为"+this.Aera);
+        return this.Aera;    }
+}
+class Square extends Shape{
+    @Override    double getAera() {
+        System.out.println("正方形的面积为"+this.Aera);
+        return this.Aera;    }
+}
+class Rectangle extends Shape{
+    @Override    double getAera() {
+        System.out.println("长方形的面积为"+this.Aera);
+        return this.Aera;    }
 }
